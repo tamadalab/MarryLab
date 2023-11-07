25e0ee4f77857b69894b4992e10a5efe45f7f209
xxxfussanxxx <mitareshi0424@gmail.com>
2023-11-07 15:13:00 +0900

    GaleSharpelyクラスにJavadocコメントを追加

--- app/src/main/java/marrylab/GaleShapleyMatching.java
+++ app/src/main/java/marrylab/GaleShapleyMatching.java
@@ -1,18 +1,34 @@
 package marrylab;
 
+/**
+ * Gale-Shapleyのアルゴリズムによって、学生を研究室に配属させるクラスです。
+ */
 public class GaleShapleyMatching {
 
 	private Table table;
 
+/**
+ * 学生を研究室に配属させるメソッドです。
+ */
+	
 	public void add() {
-
+		
 	}
 
+
+/**
+ * 溢れた学生をリストから削除するメソッドです。
+ */
 	public void remove() {
 
 	}
 
-	public boolean hasUnassignedStudents() {
+
+/**
+ * 配属されていない学生がいるかをチェックするメソッド。
+ * @return 未配属がいるかの真偽
+ */
+	public Boolean hasUnassignedStudents() {
 		return false;
 	}
 
