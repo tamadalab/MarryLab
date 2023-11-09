7bc412142768b152489c35be8dfc25d9e64738bc
速水健杜 <hayami527kento@gmail.com>
2023-11-09 13:37:04 +0900

    Lab,Tableの更新

--- app/src/main/java/marrylab/Laboratory.java
+++ app/src/main/java/marrylab/Laboratory.java
@@ -1,27 +1,66 @@
 package marrylab;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+import marrylab.Student;
+/**
+ * 研究室に関する情報を管理するクラス
+ */
 public class Laboratory {
 
+	/**
+	 * 生徒の総合点を管理するマップを保持するフィールド
+	 */
 	private HashMap<Integer, Double> studentPoint;
 
+	/**
+	 * 生徒の希望順位を保持するフィールド
+	 */
 	private List<Student> studentList;
 
+	/**
+	 * 研究室のコース点を保持するフィールド
+	 */
 	private HashMap<String, Integer> coursePoint;
 
+	/**
+	 * 研究室名を保持するフィールド
+	 */
 	private String name;
 
+	/**
+	 * 学生ごとに付与される教員展を保持するフィールド
+	 */
 	private HashMap<Integer, Double> labScore;
 
+	/**
+	 * 研究室の最大配属人数を保持するフィールド
+	 */
 	private Integer capacity;
 
 	private Student student;
 
-	public Integer updateCapacity() {
-		return null;
+	/**
+	 * 研究室のコンストラクタ
+	 */
+	public void Laboratory(){
+
 	}
 
+	/**
+	 * 研究室の最大配属人数を変更するメソッド
+	 */
+	public void updateCapacity() {
+
+	}
+
+	/**
+	 * 生徒の希望順位を総合点をもとに並べ替える
+	 */
 	public void sortStudent() {
 
 	}
-
 }
--- app/src/main/java/marrylab/Table.java
+++ app/src/main/java/marrylab/Table.java
@@ -1,9 +1,30 @@
 package marrylab;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import marrylab.Student;
+import marrylab.Laboratory;
+/**
+ * 研究室と生徒の情報を管理するクラス
+ */
 public class Table {
 
-	private HashMap<String, Laboratory> laboratoryList;
+	/**
+	 * 研究室の集合を保持するフィールド
+	 */
+	private Map<String, Laboratory> laboratoryList;
 
-	private HashMap<Integer, Student> studentsList;
+	/**
+	 * 生徒の集合を保持するフィールド
+	 */
+	private Map<Integer, Student> studentsList;
 
+	/**
+	 * コンストラクタ
+	 */
+	public void table(){
+		Map<String, Laboratory> laboratoryList = new HashMap<String, Laboratory>();
+		Map<Integer, Student> studentsList = new HashMap<Integer, Student>();
+	}
 }
