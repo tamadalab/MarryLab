86ac10ea27323db942ac27125fae6e0b2f356070
速水健杜 <hayami527kento@gmail.com>
2023-11-21 01:31:00 +0900

    readStudentGPA実装

--- app/src/main/java/marrylab/Reader.java
+++ app/src/main/java/marrylab/Reader.java
@@ -1,14 +1,31 @@
 package marrylab;
 
+import java.util.List;
+
 public class Reader extends IO {
     public Reader(Table table, String filePass) {
 		super(table, filePass);
-    }
-
-	/**
-	 * CSVファイルを読み込むメソッドです。
-	 */
-	public void read() {
-		this.CSVtoList();
 	}
+
+
+	public void readStudentGPA(String filePath) {
+		this.CSVtoList(filePath).forEach((column) -> {
+			try {
+				// column配列の長さを確認
+				if (column.length >= 4) {
+					Integer studentID = Integer.parseInt(column[0]);
+					String studentName = column[1];
+					Double gpa = Double.parseDouble(column[3]);
+	
+					if ("○".equals(column[2])) {
+						this.table.studentMap().put(studentID, new Student(studentName, gpa));
+					}
+				}
+			} catch (NumberFormatException e) {
+				// 数値変換での例外をキャッチし、適切に処理
+				e.printStackTrace(); // または他のエラー処理
+			}
+		});
+	}
+	
 }
