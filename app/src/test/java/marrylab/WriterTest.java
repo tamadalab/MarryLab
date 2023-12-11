fa5c5d87c2147fe9878a827cd82482d04e7080fb
xxxfussanxxx <mitareshi0424@gmail.com>
2023-12-11 14:48:09 +0900

    ReaderTestとWriterTestの変更

---
+++ app/src/test/java/marrylab/WriterTest.java
@@ -0,0 +1,13 @@
+package marrylab;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class WriterTest {
+	@Test
+	public void testWriter() throws IOException {
+		
+	}
+}
